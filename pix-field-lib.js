ad9a719ae33639490f0964a21d020558ce130c1e
BobB <bob.buehler@gmail.com>
2012-07-21 07:26:32 -0400

    indentation changed to 2

--- pix-field-lib.js
+++ pix-field-lib.js
@@ -1,65 +1,65 @@
 var pix_field_lib = function() {
-    return {
-        // Grab json, parse, and call me back. No callback on error.
-        read_json : function(path, callback) {
-            var first = true; // only invoke callback on first response
-            var READY = 4;
-            var txtFile = new XMLHttpRequest();
-            txtFile.open("GET", path, true);
-            txtFile.onreadystatechange = function() {
-                if (first && txtFile.readyState == READY) {
-                    first = false;
-                    callback(JSON.parse(txtFile.responseText));
-                }
-            };
-            txtFile.send(null);
-        },
-
-        // Returns a function(callback) for chaining animation frames.
-        animation_frame_requester : function() {
-            return window.requestAnimationFrame ||
-                window.webkitRequestAnimationFrame ||
-                window.mozRequestAnimationFrame ||
-                window.oRequestAnimationFrame ||
-                window.msRequestAnimationFrame ||
-                function(callback) {
-                    window.setTimeout(callback, 1000 / 60);
-                };
-        },
-
-        // Resets a context to the standard transform and clears it.
-        reset_context : function(context) {
-            context.setTransform(1, 0, 0, 1, 0, 0);
-            context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-        },
-
-        // parses a comma separated list of arrays
-        parse_pix_array : function(pix_text) {
-            return JSON.parse('{"pix":[' + pix_text + ']}').pix;
-        },
-
-        stringify_pix_array : function(pix) {
-            var buf = [];
-            pix.forEach(function(p) {
-                buf[buf.length] = '[' + p[0] + ',' + p[1] + ',"' + p[2] + '"]';
-            });
-            return buf.join(',\n');
-        },
-
-        // Draws a pix field at the current position of the context, rotated.
-        render_pix_array : function(context, pix, angle) {
-            context.rotate(angle);
-            pix.forEach(function(p) {
-                context.save();
-                context.translate(p[0], p[1]);
-                context.rotate(-angle);
-                context.fillStyle = p[2];
-                context.fillRect(-0.5, -0.5, 1, 1);
-                context.restore();
-            });
-            context.rotate(-angle);
+  return {
+    // Grab json, parse, and call me back. No callback on error.
+    read_json : function(path, callback) {
+      var first = true; // only invoke callback on first response
+      var READY = 4;
+      var txtFile = new XMLHttpRequest();
+      txtFile.open("GET", path, true);
+      txtFile.onreadystatechange = function() {
+        if (first && txtFile.readyState == READY) {
+          first = false;
+          callback(JSON.parse(txtFile.responseText));
         }
-    };
+      };
+      txtFile.send(null);
+    },
+
+    // Returns a function(callback) for chaining animation frames.
+    animation_frame_requester : function() {
+      return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function(callback) {
+          window.setTimeout(callback, 1000 / 60);
+        };
+    },
+
+    // Resets a context to the standard transform and clears it.
+    reset_context : function(context) {
+      context.setTransform(1, 0, 0, 1, 0, 0);
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    },
+
+    // parses a comma separated list of arrays
+    parse_pix_array : function(pix_text) {
+      return JSON.parse('{"pix":[' + pix_text + ']}').pix;
+    },
+
+    stringify_pix_array : function(pix) {
+      var buf = [];
+      pix.forEach(function(p) {
+        buf[buf.length] = '[' + p[0] + ',' + p[1] + ',"' + p[2] + '"]';
+      });
+      return buf.join(',\n');
+    },
+
+    // Draws a pix field at the current position of the context, rotated.
+    render_pix_array : function(context, pix, angle) {
+      context.rotate(angle);
+      pix.forEach(function(p) {
+        context.save();
+        context.translate(p[0], p[1]);
+        context.rotate(-angle);
+        context.fillStyle = p[2];
+        context.fillRect(-0.5, -0.5, 1, 1);
+        context.restore();
+      });
+      context.rotate(-angle);
+    }
+  };
 }();
 
 Math.TwoPI = Math.PI * 2;
--- pix-field-viewer.js
+++ pix-field-viewer.js
@@ -1,89 +1,90 @@
 var Viewer = function() {
-    var _canvas,
-        _context,
-        _data,
-        _pix_name,
-        _zoom = 10,
-        _zoom_min = 1,
-        _zoom_max = 100,
-        _rotate = 0,
-        _rotate_min = -180,
-        _rotate_max = 180,
-        _mouse_last_position;
+  var
+    _canvas,
+    _context,
+    _data,
+    _pix_name,
+    _zoom = 10,
+    _zoom_min = 1,
+    _zoom_max = 100,
+    _rotate = 0,
+    _rotate_min = -180,
+    _rotate_max = 180,
+    _mouse_last_position;
 
-    return {
-        init : function(canvas, data) {
-            _canvas = canvas;    
-            _data = data;
-            _context = _canvas.getContext("2d");
-        },
-        
-        pix_name_change : function(name) {
-            _pix_name = name;
-        },
-        
-        pix_to_string : function() {
-            return pix_field_lib.stringify_pix_array(_data[_pix_name].pix);
-        },
-        
-        pix_data_change : function(data) {
-            _data[_pix_name].pix = data;
-        },
-        
-        mouse_move : function(position, bnt1_down) {
-            if (bnt1_down) {
-                if (_mouse_last_position) {
-                    _zoom -= position.y - _mouse_last_position.y;
-                    _zoom = Math.max(Math.min(_zoom, _zoom_max), _zoom_min);
-                    _rotate += position.x - _mouse_last_position.x;
-                    _rotate = Math.max(Math.min(_rotate, _rotate_max), _rotate_min);
-                    _mouse_last_position = position;
-                }
-                _mouse_last_position = position;
-            } else {
-                _mouse_last_position = undefined;
-            }
-        },
-        
-        draw : function() {
-            pix_field_lib.reset_context(_context);
-            _context.fillStyle = "black";
-            _context.fillRect(0, 0, _canvas.width, _canvas.height);
-            _context.translate(_canvas.width / 2, _canvas.height / 2);
-            _context.scale(_zoom, _zoom);
-            pix_field_lib.render_pix_array(_context, _data[_pix_name].pix, _rotate * Math.PI / 180);            
+  return {
+    init : function(canvas, data) {
+      _canvas = canvas;
+      _data = data;
+      _context = _canvas.getContext("2d");
+    },
+
+    pix_name_change : function(name) {
+      _pix_name = name;
+    },
+
+    pix_to_string : function() {
+      return pix_field_lib.stringify_pix_array(_data[_pix_name].pix);
+    },
+
+    pix_data_change : function(data) {
+      _data[_pix_name].pix = data;
+    },
+
+    mouse_move : function(position, bnt1_down) {
+      if (bnt1_down) {
+        if (_mouse_last_position) {
+          _zoom -= position.y - _mouse_last_position.y;
+          _zoom = Math.max(Math.min(_zoom, _zoom_max), _zoom_min);
+          _rotate += position.x - _mouse_last_position.x;
+          _rotate = Math.max(Math.min(_rotate, _rotate_max), _rotate_min);
+          _mouse_last_position = position;
         }
-    };
+        _mouse_last_position = position;
+      } else {
+        _mouse_last_position = undefined;
+      }
+    },
+
+    draw : function() {
+      pix_field_lib.reset_context(_context);
+      _context.fillStyle = "black";
+      _context.fillRect(0, 0, _canvas.width, _canvas.height);
+      _context.translate(_canvas.width / 2, _canvas.height / 2);
+      _context.scale(_zoom, _zoom);
+      pix_field_lib.render_pix_array(_context, _data[_pix_name].pix, _rotate * Math.PI / 180);
+    }
+  };
 }();
 
 function change_select() {
-    Viewer.pix_name_change(document.getElementById("select").value);
-    Viewer.draw();
-    document.getElementById("pix").value = Viewer.pix_to_string();
+  Viewer.pix_name_change(document.getElementById("select").value);
+  Viewer.draw();
+  document.getElementById("pix").value = Viewer.pix_to_string();
 }
 
 function change_text() {
-    Viewer.pix_data_change(pix_field_lib.parse_pix_array(document.getElementById("pix").value));
-    Viewer.draw();
+  Viewer.pix_data_change(pix_field_lib.parse_pix_array(document.getElementById("pix").value));
+  Viewer.draw();
 }
 
 function on_mouse_move(ev) {
-    Viewer.mouse_move({x : ev.offsetX, y : ev.offsetY}, ev.which === 1);
-    Viewer.draw();
+  Viewer.mouse_move({x : ev.offsetX, y : ev.offsetY}, ev.which === 1);
+  Viewer.draw();
 }
 
 window.onload = function() {
-    var canvas = document.getElementById("canvas");     
-    pix_field_lib.read_json("data.json", function(data) {
-        Viewer.init(canvas, data);
-        var select = document.getElementById("select");
-        for (var prop in data) {
-            if (data[prop].pix) {
-                select.options[select.options.length] = new Option(prop, prop);
-            }
-        }
-        select.options[0].selected = true;
-        change_select();
-    });
-    canvas.onmousemove = on_mouse_move;
+  var canvas = document.getElementById("canvas");
+  pix_field_lib.read_json("data.json", function(data) {
+    Viewer.init(canvas, data);
+    var select = document.getElementById("select");
+    for (var prop in data) {
+      if (data[prop].pix) {
+        select.options[select.options.length] = new Option(prop, prop);
+      }
+    }
+    select.options[0].selected = true;
+    change_select();
+  });
+  canvas.onmousemove = on_mouse_move;
 };
