e0d2cc9cc1c71bcb7709f019df223625a76d1047
BobB <bob.buehler@gmail.com>
2012-08-21 15:27:28 -0400

    moved keyboard tracking to pix_field_lib

--- pix-field-game.js
+++ pix-field-game.js
@@ -1,36 +1,17 @@
-var KeyBoard = function() {
-  var _state = {};
-  document.onkeydown = function(ev) {
-    _state[ev.which] = true;
-  };
-  document.onkeyup = function(ev) {
-    _state[ev.which] = false;
-  };
-  return {
-    SPACE : 32,
-    LEFT : 37,
-    RIGHT : 39,
-    DOWN : 40,
-    get : function(key) {
-      return _state[key];
-    }
-  };
-}();
-
-var Loop = function() {
+var pix_field_loop = function() {
   var
     _canvas,
     _context,
-    _request_animation_frame,
     _zoom = 5,
+    _keyboard_state, SPACE = 32, LEFT = 37, RIGHT = 39, DOWN = 40,
+    _request_animation_frame,
     _last_time,
-    _lifter,
-    _helicopter,
-    _hover_zone;
+    _lifter, _helicopter, _hover_zone;
   return {
     init : function(canvas) {
       _canvas = canvas;
       _context = _canvas.getContext("2d");
+      _keyboard_state = pix_field_lib.key_state_tracker(document, "onkeydown", "onkeyup");
       _request_animation_frame = pix_field_lib.animation_frame_requester();
       _last_time = new Date().getTime();
       _lifter = pix_field_lib.create_lifter(_canvas.width / 2 / _zoom, _canvas.height / 2 / _zoom);
@@ -41,9 +22,9 @@ var Loop = function() {
       var time = new Date().getTime();
       var delta_time = (time - _last_time) / 1000;
       _last_time = time;
-      _lifter.step(delta_time, KeyBoard.get(KeyBoard.DOWN), KeyBoard.get(KeyBoard.SPACE), KeyBoard.get(KeyBoard.LEFT), KeyBoard.get(KeyBoard.RIGHT));
+      _lifter.step(delta_time, _keyboard_state.get(DOWN), _keyboard_state.get(SPACE), _keyboard_state.get(LEFT), _keyboard_state.get(RIGHT));
       _lifter.bound(0, 0, _canvas.width / _zoom, _canvas.height / _zoom);
-      if (!(KeyBoard.get(KeyBoard.DOWN) && _lifter.get_frozen())) {
+      if (!(_keyboard_state.get(DOWN) && _lifter.get_frozen())) {
         _helicopter.step(delta_time, _lifter.get_thrust_percent());
       }
       _hover_zone.step(delta_time, _hover_zone.contains(_lifter.get_x(), _lifter.get_y()));
@@ -53,13 +34,13 @@ var Loop = function() {
       _helicopter.draw(_context, _lifter.get_x(), _lifter.get_y(), _lifter.get_angle());
       document.getElementById("fps").innerHTML = "fps: " + (1 / delta_time);
       _request_animation_frame(function() {
-        Loop.iterate();
+        pix_field_loop.iterate();
       });
     }
   };
 }();
 
 window.onload = function() {
-  Loop.init(document.getElementById("canvas"));
-  Loop.iterate();
+  pix_field_loop.init(document.getElementById("canvas"));
+  pix_field_loop.iterate();
 };
